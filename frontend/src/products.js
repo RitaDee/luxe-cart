79529ed34338c25cbc3d14144a6f7c629adcd8fd
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-10-10 20:09:03 +0100

    refactor: modify product.js file

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,8 +1,16 @@
+import airpod from './images/airpods.jpg';
+import alexa from './images/alexa.jpg';
+import camera from './images/camera.jpg';
+import mouse from './images/mouse.jpg';
+import phone from './images/phone.jpg';
+import playstation from './images/playstation.jpg';
+import sample from './images/sample.jpg';
+
 const products = [
     {
         _id: '1',
         name: 'Airpod',
-        image: '../public/images/airpods.jpg',
+        image: airpod,
         description: 'Wireless Airpods with Lightning Charging Case Included. Over 24 Hours of Battery Life, Effortless Setup. Bluetooth Headphones for iPhone',
         brand: 'Apple',
         category: 'Electronics',
@@ -14,7 +22,7 @@ const products = [
     {
         _id: '2',
         name: 'Alexa',
-        image: '../public/images/alexa.jpg',
+        image: alexa,
         description: 'Alexa is an intelligent personal assistant that makes use of machine learning and artificial intelligence. It is designed to be always on and woken with a voice command',
         brand: 'Amazon',
         category: 'Electronics',
@@ -26,7 +34,7 @@ const products = [
     {
         _id: '3',
         name: 'Camera',
-        image: '../public/images/camera.jpg',
+        image: camera,
         description: '4K Digital Camera for Photography Autofocus, 48MP Vlogging Camera with SD Card Anti-Shake',
         brand: 'Samsung',
         category: 'Electronics',
@@ -36,41 +44,65 @@ const products = [
         numReviews: '20',
     },
     {
-        _id: '1',
+        _id: '4',
+        name: 'Mouse',
+        image: mouse,
+        description: 'Wireless Airpods with Lightning Charging Case Included. Over 24 Hours of Battery Life, Effortless Setup. Bluetooth Headphones for iPhone',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '90.00',
+        CountInStock: '10',
+        rating: '4.5',
+        numReviews: '8',
+    },
+    {
+        _id: '5',
+        name: 'Phone',
+        image: phone,
+        description: 'Alexa is an intelligent personal assistant that makes use of machine learning and artificial intelligence. It is designed to be always on and woken with a voice command',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '75.00',
+        CountInStock: '8',
+        rating: '4.2',
+        numReviews: '13',
+    },
+    {
+        _id: '6',
+        name: 'Play station',
+        image: playstation,
+        description: '4K Digital Camera for Photography Autofocus, 48MP Vlogging Camera with SD Card Anti-Shake',
+        brand: 'Samsung',
+        category: 'Electronics',
+        price: '100.00',
+        CountInStock: '12',
+        rating: '5.0',
+        numReviews: '20',
+    },
+     {
+        _id: '7',
+        name: 'Sample',
+        image: sample,
+        description: '4K Digital Camera for Photography Autofocus, 48MP Vlogging Camera with SD Card Anti-Shake',
+        brand: 'Samsung',
+        category: 'Electronics',
+        price: '100.00',
+        CountInStock: '12',
+        rating: '5.0',
+        numReviews: '20',
+    },
+     {
+        _id: '8',
         name: 'Airpod',
-        image: '../public/images/airpods.jpg',
+        image: airpod,
         description: 'Wireless Airpods with Lightning Charging Case Included. Over 24 Hours of Battery Life, Effortless Setup. Bluetooth Headphones for iPhone',
         brand: 'Apple',
         category: 'Electronics',
-        price: '90.00',
+        price: '85.00',
         CountInStock: '10',
         rating: '4.5',
         numReviews: '8',
-    },
-    {
-        _id: '2',
-        name: 'Alexa',
-        image: '../public/images/alexa.jpg',
-        description: 'Alexa is an intelligent personal assistant that makes use of machine learning and artificial intelligence. It is designed to be always on and woken with a voice command',
-        brand: 'Amazon',
-        category: 'Electronics',
-        price: '75.00',
-        CountInStock: '8',
-        rating: '4.2',
-        numReviews: '13',
-    },
-    {
-        _id: '3',
-        name: 'Camera',
-        image: '../public/images/camera.jpg',
-        description: '4K Digital Camera for Photography Autofocus, 48MP Vlogging Camera with SD Card Anti-Shake',
-        brand: 'Samsung',
-        category: 'Electronics',
-        price: '100.00',
-        CountInStock: '12',
-        rating: '5.0',
-        numReviews: '20',
-    },
+    }
 ]
 
 export default products;
